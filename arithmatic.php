4efd2c080eddaf557929e3cb3cd2200d1df8a5a6
Shahin <meshahin007@gmail.com>
2019-02-25 20:48:36 +0600

    commit

--- arithmatic.php
+++ arithmatic.php
@@ -1,8 +1,16 @@
 <?php
 
-$number = 12;
+// $number = 12;
 
 
-$number = (13-3) * 12;
+// $number = (13-3) * 12;
 
-echo $number;
+// echo $number;
+
+$n = 12;
+
+if($n > 10) {
+    echo "$n is grater than 10";
+} else{
+     echo "$n is less than 10";
+}
