de0905516ca7b4b158fe55b825dd14fe33a1e13d
Shahin <meshahin007@gmail.com>
2019-02-27 13:37:05 +0600

    commit

--- arithmatic.php
+++ arithmatic.php
@@ -7,10 +7,72 @@
 
 // echo $number;
 
-$n = 12;
+// $n = 12;
 
-if($n > 10) {
-    echo "$n is grater than 10";
-} else{
-     echo "$n is less than 10";
-}
+// if($n > 10) {
+//     echo "$n is grater than 10";
+// } else{
+//      echo "$n is less than 10";
+// }
+
+
+// $n = 10;
+// $r = 11;
+
+// if($n != $r) {
+//     echo "$n equal to $r";
+// } else{
+//     echo "$n not equal to $r";
+// }
+
+// $alam = 100;
+// $rohim = 100;
+
+// if($alam < $rohim) {
+//     echo "alam has less money of rohim";
+// } elseif ($alam == $rohim) {
+//    echo "alam has same amount of rohim";
+// }elseif ($alam > $rohim) {
+//    echo "alam has more amount of rohim";
+// }
+
+
+
+$f = false || true;
+$e = false or true;
+
+
+var_dump($f, $e);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
